30cfc0bba5f024f4820e66c773e16f5c7a2b2210
Deryck Henson <deryck.henson@gmail.com>
2015-07-11 06:00:10 -0500

    git-notify will stay in the repo

---
+++ git-notify.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+  app = express(),
+  cmd = require("child_process").spawn;
+
+app.post('/git', function(req, res) {
+  var git;
+  if (!req.headers["x-hub-signature"]) {
+    res.end();
+    return false;
+  }
+  git = cmd('git', ['pull', '-u', 'origin', 'master']);
+  console.log(new Date());
+  git.stdout.on("data", function(data) {
+    console.log("" + data);
+  });
+  res.end();
+});
+
+app.listen(9787);
