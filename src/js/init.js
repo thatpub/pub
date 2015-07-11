05522cfd3b7d6e530293c3c0ce8cb4c3433c343c
Deryck Henson <deryck.henson@gmail.com>
2015-07-11 10:18:44 -0500

    re-ordering of files and internals.  code grew a little but I think its from my new uglifyjs options

--- src/js/init.js
+++ src/js/init.js
@@ -1,133 +1,38 @@
-;(function(window, document, app, undefined) {
-  "use strict";
-  app.handleResponse = function ( httpRequest, action ) {
-    var that = this,
-      response = JSON.parse(httpRequest.responseText),
-      content = response[0] || null,
-      meta = response[1] || null,
-      expires, a = 0, b = 0, reveals, rl;
+"use strict";
 
-    this.isDone = true;
+addEvent(app.send_, "click", app.searchStart);
 
-    if ((content && content.hits.total === 0) &&
-        (meta && meta.hits.total === 0)) {
-      this.isFailure = true;
-      this.infiniScroll = false;
-      document.cookie = "placeContent=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-      document.cookie = "placeMeta=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-      fastdom.write(function() {
-        that.message_.innerHTML = "Your search returned no results.  Give \'er another go.";
-      });
-      swapClass(this.loader_, "", regLoad);
-      this.xIcon_.style.display = "none";
-      swapClass(this.searchWrap_, "failed", regFail);
-      return false;
-    }
+addEvent(app.query_, "focus", app.handleFocus);
 
-    expires = new Date(Date.now() + 60000)
-    expires = expires.toUTCString();
-    fastdom.defer(function() {
-      if ( that.resetSearch ) {
-        clearTimeout(that.resetSearch);
-      }
-      that.resetSearch = setTimeout(function() {
-        // Server is only caching the scroll/page position for 1 minute.
-        // Sorry bucko.
-        swapClass(that.moreContent_, "hidden", regHidden);
-        that.loading.stillMore = false;
-      }, 60000);
-    });
+addEvent(app.query_, "keypress", app.handleInput);
 
-    if ( content ) {
-      var currentContent = content.hits.hits.length;
-      this.placeContent = content._scroll_id;
-      document.cookie = "placeContent=" + this.placeContent + "; expires=" + expires;
-      if ( action !== "more" ) {
-        var currentRelatives = content.aggregations.related_doc.buckets.length;
-        window.scroll(0, 0);
-        b = 0;
-        for (; b < currentContent; ++b) {
-          this.scoresContent[b] = content.hits.hits[b]._score;
-        }
-        for (b = 0; b < currentRelatives; ++b) {
-          this.scoresRelatives[b] = content.aggregations.related_doc.buckets[b].score;
-        }
-        fastdom.write(function() {
-          that.term_.innerHTML = that.term;
-          that.count_.innerHTML = content.hits.hits.length;
-          that.total_.innerHTML = content.hits.total;
-          that.related_.innerHTML = that.renderResults("relatives", content.aggregations.related_doc.buckets);
-          that.results_.innerHTML = that.renderResults("results", content.hits.hits);
-        });
-      }
-      else {
-        for (b = 0; b < currentContent; ++b) {
-          this.scoresContent.push(content.hits.hits[b]._score);
-        }
-        fastdom.write(function() {
-          that.count_.innerHTML = that.scoresContent.length;
-          that.results_.innerHTML += that.renderResults("results", content.hits.hits);
-        });
-      }
+addEvent(document, "keyup", app.closeModal);
 
-      fastdom.defer(function() {
-        that.relatedRect = that.related_.getBoundingClientRect();
-        that.bodyRect = document.body.getBoundingClientRect();
-        that.stickyBarPosition = Math.abs(that.relatedRect.top) + Math.abs(that.bodyRect.top) + Math.abs(that.relatedRect.height);
-      });
+addEvent(app.moreContent_, "click", app.more);
 
-      fastdom.defer(function() {
-        reveals = that.results_.querySelectorAll(".reveal-text");
-        rl = reveals.length;
-        a = 0;
-        for (; a < rl; ++a) {
-          addEvent(reveals[a], "click", that.revealText);
-        }
-      });
-
-      if ( content.hits.hits.length < 20 ) {
-        this.loading.stillMore = false;
-        swapClass(this.moreContent_, "hidden", regHidden);
-      }
-      else {
-        this.loading.stillMore = true;
-        swapClass(this.moreContent_, "", regHidden);
-      }
-    }
-
-    if ( meta ) {
-      this.placeMeta = meta._scroll_id;
-      document.cookie = "placeMeta=" + this.placeMeta + "; expires=" + expires;
-    }
-
-    fastdom.defer(function() {
-      that.resultsRect = that.results_.getBoundingClientRect();
-      that.loading.currentHeight = Math.abs(that.resultsRect.height);
-      that.loading.now = false;
-    });
-
-    this.searchBoxToggle("close");
-    swapClass(this.loader_, "", regLoad);
-  };
-  app.submitQuery = function ( type, action, contentPager, metaPager ) {
-    var that = this,
-      request = new XMLHttpRequest(),
-      query = this.term,
-      url = "//that.pub/find/" + type + "/" + action,
-      data = {
-        t:querySetup(query),
-        g:contentPager,
-        s:metaPager
-      },
-      dataString = JSON.stringify(data);
-    request.onreadystatechange = function() {
-      if (request.readyState === 4 && request.status === 200) {
-        that.handleResponse(request, action);
-      }
-    };
-
-    request.open("POST", url, true);
-    request.setRequestHeader("Content-type", "application/json");
-    request.send(dataString);
+addEvent(app.searchRestore_, "click", function ( event ) {
+  event.preventDefault();
+  if ( app.isSearchBoxOpen === true && app.isDone === true && app.isFailure !== true) {
+    app.searchBoxToggle("close");
   }
-})(this, this.document, app);
+  else {
+    app.searchBoxToggle("open");
+    app.query_.value = app.term;
+    app.query_.focus();
+  }
+  return false;
+});
+
+addEvent(app.infiniScroll_, "change", app.infini);
+
+addEvent(window, "scroll", app.scrollWheeler);
+
+addEvent(window, "load", function () {
+  app.isSearchBoxOpen = true;
+  app.isDone = false;
+  app.query_.focus();
+});
+
+addEvent(document, "DOMContentLoaded", function() {
+  FastClick.attach(app.wrap_);
+});
