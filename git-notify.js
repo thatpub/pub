1b4582e3676bf6ef16b086dc797919cd4ec3c222
Deryck Henson <dhenson@viztek.net>
2016-06-18 03:18:52 -0400

    updating packages and ignore files, adding extra cmds to run when new stuff from git repo comes through

--- git-notify.js
+++ git-notify.js
@@ -1,20 +1,37 @@
-var express = require("express"),
-  app = express(),
-  cmd = require("child_process").spawn;
+'use strict';
 
-app.post('/git', function(req, res) {
-  var git;
-  if (!req.headers["x-hub-signature"]) {
-    console.warn(`Failed attempt to access the /git URL at ${new Date()}`, req.headers);
+const express = require("express");
+const app = express();
+const cmd = require("child_process").spawn;
+
+app.post('/git', function ( req, res ) {
+    if ( !req.headers[ "x-hub-signature" ] ) {
+        console.log(`Failed attempt to access the /git URL at ${new Date()}`, req.headers);
+        res.end();
+        return false;
+    }
+    console.log(`git stash and pull ran at ${new Date()}`);
+    cmd('git', [ 'stash' ]);
+    const git = cmd('git', [ 'pull', '-u', 'origin', 'master' ]);
+    git.stdout.on("data", function ( data ) {
+        console.log(`${data}`);
+    });
+    console.log(`bower install ran at ${new Date()}`);
+    const bower = cmd('bower', [ 'install' ]);
+    bower.stdout.on("data", function ( data ) {
+        console.log(`${data}`);
+    });
+    console.log(`npm install ran at ${new Date()}`);
+    const npm = cmd('npm', [ 'install' ]);
+    npm.stdout.on("data", function ( data ) {
+        console.log(`${data}`);
+    });
+    console.log(`grunt ran at ${new Date()}`);
+    const grunt = cmd('grunt');
+    grunt.stdout.on("data", function ( data ) {
+        console.log(`${data}`);
+    });
     res.end();
-    return false;
-  }
-  git = cmd('git', ['pull', '-u', 'origin', 'master']);
-  console.log(new Date());
-  git.stdout.on("data", function(data) {
-    console.log("" + data);
-  });
-  res.end();
 });
 
 app.listen(9787);
