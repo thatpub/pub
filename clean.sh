381f3e872d78ec0ca46aec448bc1e8fed95687b2
Deryck Henson <dhenson@viztek.net>
2016-08-06 16:57:31 -0400

    shell yelling about a period.

--- clean.sh
+++ clean.sh
@@ -2,11 +2,11 @@
 
 for DIR in "$@"
 do
-    if [ -d $DIR ];then
-        [ -f $DIR/index.*html ] && rm $DIR/index.*html
-        [ -d $DIR/css ] && rm -rf $DIR/css/* || mkdir $DIR/css
-        [ -d $DIR/js ] && rm -rf $DIR/js/* || mkdir $DIR/js
+    if [ -d "${DIR}" ];then
+        [ -f "${DIR}/index.*html" ] && rm "${DIR}/index.*html"
+        [ -d "${DIR}/css" ] && rm -rf "${DIR}/css/*" || mkdir "${DIR}/css"
+        [ -d "${DIR}/js" ] && rm -rf "${DIR}/js/*" || mkdir "${DIR}/js"
     else
-        mkdir -p $DIR/css $DIR/js
+        mkdir -p "${DIR}/css" "${DIR}/js"
     fi
 done
