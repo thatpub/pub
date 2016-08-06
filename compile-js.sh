7660ba9e709b05a344f04a399083f528aa1292b1
Deryck Henson <dhenson@viztek.net>
2016-08-06 17:36:02 -0400

    consolidate and separate

--- compile-js.sh
+++ compile-js.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-JS_EVENT=src/lib/event/build/event.min.js
-JS_FASTDOM=src/lib/fastdom/fastdom.min.js
-JS_STRICTDOM=src/lib/fastdom/fastdom-strict.js
-JS_FASTCLICK=src/lib/fastclick/lib/fastclick.js
-JS_LODASH=src/js/lodash.custom.min.js
-JS_IMMUTABLE=src/lib/immutable/dist/immutable.min.js
+if [ "$1" = "dist" ];then
+    JS_EVENT=src/lib/event/build/event.min.js
+    JS_FASTDOM=src/lib/fastdom/fastdom.min.js
+    JS_FASTCLICK=src/lib/fastclick/lib/fastclick.js
+    JS_LODASH=src/js/lodash.custom.min.js
+    JS_IMMUTABLE=src/lib/immutable/dist/immutable.min.js
+else
+    JS_EVENT=src/lib/event/build/event.js
+    JS_FASTDOM="src/lib/fastdom/fastdom.js src/lib/fastdom/fastdom-strict.js"
+    JS_FASTCLICK=src/lib/fastclick/lib/fastclick.js
+    JS_LODASH=src/js/lodash.custom.js
+    JS_IMMUTABLE=src/lib/immutable/dist/immutable.js
+fi
 
 JS_HELPERS=src/js/helpers.js
 JS_TEMPLATES=src/js/templates.js
@@ -15,6 +22,7 @@ LIBS="${JS_EVENT} ${JS_FASTDOM} ${JS_FASTCLICK} ${JS_LODASH} ${JS_IMMUTABLE}"
 SRC="${JS_HELPERS} ${JS_TEMPLATES} ${JS_APP}"
 
 INPUT="${LIBS} ${SRC}"
+
 COMPRESS_OPTIONS="--compress unsafe,keep_fargs,drop_console,if_return,join_vars,cascade,booleans,loops,unused,comparisons,conditionals,drop_debugger,dead_code,properties,sequences"
 OTHER_ARGUMENTS="--screw-ie8 --mangle --comments"
 OUTPUTS="--source-map dist/js/script.js.map --output dist/js/script.js"
@@ -22,5 +30,5 @@ OUTPUTS="--source-map dist/js/script.js.map --output dist/js/script.js"
 if [ "$1" = "dist" ];then
     uglifyjs ${INPUT} ${COMPRESS_OPTIONS} ${OTHER_ARGUMENTS} ${OUTPUTS}
 else
-    uglifyjs ${INPUT} ${JS_STRICTDOM} ${OUTPUTS} --beautify
+    uglifyjs ${INPUT} ${OUTPUTS}
 fi
