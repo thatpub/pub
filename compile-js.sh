89081064f704e3ccbb29d4e27e1ed208cf882d82
Deryck Henson <deryck.henson@gmail.com>
2016-08-06 19:51:05 -0500

    converted to a git-centered module system since bower pissed meoff

--- compile-js.sh
+++ compile-js.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
 if [ "$1" = "dist" ];then
-    JS_EVENT=src/lib/event/build/event.min.js
     JS_FASTDOM=src/lib/fastdom/fastdom.min.js
     JS_FASTCLICK=src/lib/fastclick/lib/fastclick.js
     JS_LODASH=src/js/lodash.custom.min.js
-    JS_IMMUTABLE=src/lib/immutable/dist/immutable.min.js
+    JS_IMMUTABLE=src/lib/immutable-js/dist/immutable.min.js
 else
-    JS_EVENT=src/lib/event/build/event.js
     JS_FASTDOM=src/lib/fastdom/fastdom.js
 #    JS_FASTDOM="src/lib/fastdom/fastdom.js src/lib/fastdom/fastdom-strict.js"
     JS_FASTCLICK=src/lib/fastclick/lib/fastclick.js
     JS_LODASH=src/js/lodash.custom.js
-    JS_IMMUTABLE=src/lib/immutable/dist/immutable.js
+    JS_IMMUTABLE=src/lib/immutable-js/dist/immutable.js
 fi
 
 JS_HELPERS=src/js/helpers.js
 JS_TEMPLATES=src/js/templates.js
 JS_APP=src/js/app.js
 
-LIBS="${JS_EVENT} ${JS_FASTDOM} ${JS_FASTCLICK} ${JS_LODASH} ${JS_IMMUTABLE}"
+LIBS="${JS_FASTDOM} ${JS_FASTCLICK} ${JS_LODASH} ${JS_IMMUTABLE}"
 SRC="${JS_HELPERS} ${JS_TEMPLATES} ${JS_APP}"
 
 INPUT="${LIBS} ${SRC}"
