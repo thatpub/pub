78074ac4dd0acd5c787a9a1fbf4369d065cbfe40
Deryck Henson <deryck.henson@gmail.com>
2016-08-06 19:43:59 -0500

    re-work of lib holding - no bower

--- clean.sh
+++ clean.sh
@@ -3,10 +3,11 @@
 for DIR in "$@"
 do
     if [ -d "${DIR}" ];then
-        [ -f "${DIR}/index.*html" ] && rm "${DIR}/index.*html"
-        [ -d "${DIR}/css" ] && rm -rf "${DIR}/css/*" || mkdir "${DIR}/css"
-        [ -d "${DIR}/js" ] && rm -rf "${DIR}/js/*" || mkdir "${DIR}/js"
+        [ -f "${DIR}/index.html" ] && rm -rf $DIR/index.html
+        [ -f "${DIR}/index.full.html" ] && rm -rf $DIR/index.full.html
+        [ -d "${DIR}/css" ] && rm -rf $DIR/css/* || mkdir $DIR/css
+        [ -d "${DIR}/js" ] && rm -rf $DIR/js/* || mkdir $DIR/js
     else
-        mkdir -p "${DIR}/css" "${DIR}/js"
+        mkdir -p $DIR/css $DIR/js
     fi
 done
--- src/lib/event/README.md
+++
@@ -1,6 +0,0 @@
-event
-=====
-
-[![Build Status](https://travis-ci.org/wilsonpage/event.png?branch=master)](https://travis-ci.org/wilsonpage/event)
-
-A tiny event library
